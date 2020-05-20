e71f2075b34c5b4f75d5df0a25fff1e77d0b0b4a
Nick Pisciotta <npisciotta@gmail.com>
2020-05-20 09:26:27 -0500

    Complete handshake by pulling security token out of header and returning security key

--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'digest/sha1'
 
 server = TCPServer.new('localhost', 2345)
 
@@ -14,7 +15,27 @@ loop do
   end
 
   STDERR.puts http_request
-  socket.close
+
+  if matches = http_request.match(/^Sec-WebSocket-Key: (\S+)/)
+    websocket_key = matches[1]
+    STDERR.puts "Websocket handshake detected with key: #{ websocket_key}"
+  else 
+    STDERR.puts "Aborting non-websocket connection"
+    socket.close
+    next
+  end 
+
+  response_key = Digest::SHA1.base64digest([websocket_key, "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"].join)
+  STDERR.puts "Reponding to handhake with key: #{response_key}"
+
+  socket.write <<-eos
+  HTTP/1.1 101 Switching Protocols
+  Upgrade: websocket
+  Connection: Upgrade
+  Sec-WebSocket-Accept: #{ response_key }
+  eos
+
+  STDERR.puts "Handshake completed"
 end
 
 
